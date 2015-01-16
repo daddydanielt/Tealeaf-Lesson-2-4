adddef79e8db166ad86f37010a44897f069dd605
DaddyDanielT <artsppdaniel@gmail.com>
2015-01-16 16:15:22 +0800

    add seeds to initial Database data

--- db/migrate/20150114172221_add_data_to_roles.rb
+++
@@ -1,7 +0,0 @@
-class AddDataToRoles < ActiveRecord::Migration
-  def change
-      Role.create(role_name: 'admin')
-      Role.create(role_name: 'user')
-      Role.create(role_name: 'guest')
-  end
-end
